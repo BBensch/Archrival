eae05a37b3ea48db2f0b246af4e4ca373a5a0938
bbensch <bbensch@live.com>
2014-11-08 11:08:14 -0500

    Frameworking for database pulls, show/hide details
    Prepping what data should look like as pulled from the future database

--- public/javascripts/MainCtrl.js
+++ public/javascripts/MainCtrl.js
@@ -1,6 +1,23 @@
 angular.module('FYS.MainCtrl' , [])
 	.controller("MainCtrl", function ($scope, $http) {
-		$scope.test = function() {
-			console.log("Button Press");
+		$scope.show = false;
+
+		$scope.sseLab = "The SSE Lab is an organization on Campus to mentor students in computer science and software engineering."
+
+		$scope.showMsg = function(id) {
+			switch (id) {
+				case 1:
+					$scope.show = true;
+					$scope.details = {
+						desc: $scope.sseLab,
+						hours: "10AM - 6PM",
+						type: "Academic"
+					}
+					break
+				default: 
+					$scope.details = {
+						desc: "Something else"
+					}
+				}
 		}
 	})
